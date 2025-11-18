24caed4c43d68f611d4f29c7548f84702e8a55e2
FxnnBT <40134@ma-web.nl>
2025-11-18 10:38:31 +0100

    canvas add

---
+++ Canvas/main.js
@@ -0,0 +1,51 @@
+        const canvas = document.getElementById("canvas");
+        const g = canvas.getContext("2d");
+        const ctx = canvas.getContext("2d");
+
+
+        
+        ctx.beginPath();
+        ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+        ctx.stroke();
+
+       
+       
+       
+       
+       
+        /*
+        g.beginPath();
+        g.moveTo(200, 80);  
+        g.lineTo(300, 150);  
+        g.lineTo(260, 260);  
+        g.lineTo(140, 260);  
+        g.lineTo(100, 150);  
+        g.closePath();
+        g.stroke();
+        */
+        /*
+        g.beginPath();
+        g.moveTo(300, 50);
+        g.lineTo(450, 350);
+        g.lineTo(150, 350);
+        g.closePath();
+        g.stroke();
+        */
+        
+        /*ctx.strokeStyle = "red";
+        ctx.fillStyle = "green";
+        ctx.rect(10, 10, 1000, 780);
+        ctx.fill();
+        ctx.stroke();
+       */
+       
+       
+       
+       
+        /*.moveTo(100, 100);
+        g.lineTo(200, 100);
+        g.lineTo(200, 200);
+        g.lineTo(100, 200);
+        g.lineTo(100, 100);
+        g.stroke();
+        */
