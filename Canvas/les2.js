b530c023350abca111dea320eb91a5ab3dfbe816
FxnnBT <40134@ma-web.nl>
2025-11-25 10:20:12 +0100

    ball

---
+++ Canvas/les2.js
@@ -0,0 +1,21 @@
+const canvas = document.getElementById("canvas");
+const g = canvas.getContext("2d");
+const ctx = canvas.getContext("2d");
+
+function ball(xPos, yPos, radius, color) {
+  g.fillStyle = color;
+  g.beginPath();
+  g.arc(xPos, yPos, radius, 0, 2 * Math.PI);
+  g.closePath();
+  g.fill();
+  g.stroke();
+}
+for (let i = 0; i < 500; i++) {
+  ball(Math.random() * 1200, Math.random() * 900, 20, "blue");
+}
+/*ball(Math.random() * 1000, Math.random() * 900, 50, "red");
+ball(Math.random() * 1000, Math.random() * 900, 40, "blue");
+ball(Math.random() * 1000, Math.random() * 900, 30, "green");
+ball(Math.random() * 1000, Math.random() * 900, 60, "purple");
+ball(Math.random() * 1000, Math.random() * 900, 20, "orange");
+*/
