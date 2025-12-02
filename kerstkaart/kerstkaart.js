1199799063d0208e8d93e521ff50db17800b73a9
FxnnBT <40134@ma-web.nl>
2025-12-02 10:47:34 +0100

    huizen en dak

--- kerstkaart/kerstkaart.js
+++ kerstkaart/kerstkaart.js
@@ -5,19 +5,59 @@ const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
 
     function boom() {
+    // stam
     g.fillStyle = "brown";
     g.fillRect(875, 700, 50, 150);
+    
+    // meerdere lagen groene driehoeken
     g.fillStyle = "green";
+    
+    // onderste laag
     g.beginPath();
-    g.moveTo(900, 400);
-    g.lineTo(700, 700);
-    g.lineTo(1100, 700);
+    g.moveTo(900, 550);
+    g.lineTo(750, 700);
+    g.lineTo(1050, 700);
+    g.closePath();
+    g.fill();
+    g.stroke();
+    
+    // middelste laag
+    g.beginPath();
+    g.moveTo(900, 450);
+    g.lineTo(780, 600);
+    g.lineTo(1020, 600);
+    g.closePath();
+    g.fill();
+    g.stroke();
+    
+    // bovenste laag
+    g.beginPath();
+    g.moveTo(900, 350);
+    g.lineTo(820, 500);
+    g.lineTo(980, 500);
+    g.closePath();
+    g.fill();
+    g.stroke();
+    
+    // ster op de top
+    g.fillStyle = "gold";
+    g.beginPath();
+    g.moveTo(900, 320);
+    g.lineTo(910, 345);
+    g.lineTo(935, 345);
+    g.lineTo(915, 360);
+    g.lineTo(925, 385);
+    g.lineTo(900, 370);
+    g.lineTo(875, 385);
+    g.lineTo(885, 360);
+    g.lineTo(865, 345);
+    g.lineTo(890, 345);
     g.closePath();
     g.fill();
     g.stroke();
     }
     
-    //ballen random kleur
+    //random kleur
     function randomcolor() {
         let r = Math.random() * 255;
         let g = Math.random() * 255;
@@ -26,27 +66,104 @@ const canvas = document.getElementById("canvas");
         return color;
     }
 
+    // vaste posities voor de ballen
+    const ballenPosities = {
+        bal1: { xpos: 820, ypos: 670 },
+        bal2: { xpos: 980, ypos: 670 },
+        bal3: { xpos: 900, ypos: 640 },
+        bal4: { xpos: 850, ypos: 580 },
+        bal5: { xpos: 950, ypos: 580 },
+        bal6: { xpos: 870, ypos: 510 },
+        bal7: { xpos: 930, ypos: 480 },
+        bal8: { xpos: 900, ypos: 400 }
+    };
 
     function ballen() {
-        ctx.fillStyle = randomcolor();
+        Object.values(ballenPosities).forEach(pos => {
+            ctx.fillStyle = randomcolor();
+            ctx.beginPath();
+            ctx.arc(pos.xpos, pos.ypos, 15, 0, Math.PI * 2);
+            ctx.fill();
+            ctx.stroke();
+        });
+    }
+
+    // functie om huisje 1 te tekenen
+    function huisje1() {
+        // voorkant
+        ctx.fillStyle = "white";
+        ctx.fillRect(100, 720, 80, 80);
+        ctx.strokeRect(100, 720, 80, 80);
+        
+        // dak
+        ctx.fillStyle = "red";
         ctx.beginPath();
-        ctx.arc(Math.random() * 300 + 750, Math.random() * 250 + 450, 15, 0, Math.PI * 2);
+        ctx.moveTo(140, 680);
+        ctx.lineTo(90, 720);
+        ctx.lineTo(190, 720);
+        ctx.closePath();
         ctx.fill();
         ctx.stroke();
     }
 
-    ballen();
-    ballen();
-    ballen();
-    ballen();
-    ballen();
-    ballen();
-    ballen();
+    function huisje2() {
+        // voorkant
+        ctx.fillStyle = "white";
+        ctx.fillRect(200, 720, 80, 80);
+        ctx.strokeRect(200, 720, 80, 80);
+        
+
+        ctx.fillStyle = "red";
+        ctx.beginPath();
+        ctx.moveTo(240, 680);
+        ctx.lineTo(190, 720);
+        ctx.lineTo(290, 720);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+    }
 
 
+    function huisje3() {
+
+        ctx.fillStyle = "white";
+        ctx.fillRect(300, 720, 80, 80);
+        ctx.strokeRect(300, 720, 80, 80);
+        
+
+        ctx.fillStyle = "red";
+        ctx.beginPath();
+        ctx.moveTo(340, 680);
+        ctx.lineTo(290, 720);
+        ctx.lineTo(390, 720);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+    }
 
 
+    function huisje4() {
+
+        ctx.fillStyle = "white";
+        ctx.fillRect(400, 720, 80, 80);
+        ctx.strokeRect(400, 720, 80, 80);
+        
+
+        ctx.fillStyle = "red";
+        ctx.beginPath();
+        ctx.moveTo(440, 680);
+        ctx.lineTo(390, 720);
+        ctx.lineTo(490, 720);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+    }
 
     boom();
-
-    //ballen tekenen
+    ballen();
+    
+    // huisjes
+    huisje1();
+    huisje2();
+    huisje3();
+    huisje4();
